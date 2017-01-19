40cc333b39dc7ead5fcba8e27cf2d956744e7bfd
Ted Callahan <CCallahanIV@gmail.com>
2017-01-18 17:13:59 -0800

    Added django-registration.

--- imagersite/imagersite/forms.py
+++ imagersite/imagersite/forms.py
@@ -3,8 +3,12 @@ from django import forms
 
 
 class UserForm(forms.ModelForm):
+    """Create a userform class for login."""
+
     password = forms.CharField(widget=forms.PasswordInput())
 
     class Meta:
+        """Subclass defines form fields."""
+
         model = User
         fields = ('username', 'password', 'email')
--- imagersite/imagersite/urls.py
+++ imagersite/imagersite/urls.py
@@ -13,17 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
 from imagersite.views import (home_view,
-                              register_view
                               )
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', home_view, name="home"),
-    url(r'^login/$', auth_views.login, {'template_name': 'imagersite/login.html'}, name="login"),
-    url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name="logout"),
-    url(r'^register/$', register_view, name="register")
+    url(r'^accounts/', include('registration.backends.hmac.urls'))
 ]
