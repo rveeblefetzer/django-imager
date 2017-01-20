98c0a64b365b7ccbb589749a33645a8e1095b012
Ted Callahan <CCallahanIV@gmail.com>
2017-01-19 18:21:12 -0800

    FIRST TEST FOR PHOTOS, YAY

--- imagersite/imager_images/tests.py
+++ imagersite/imager_images/tests.py
@@ -9,28 +9,31 @@ fake = Faker()
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEST_IMAGE_PATH = os.path.join(BASE_DIR, 'imager_images/test.png')
 
+
 class ImageTestCase(TestCase):
     """The test runner for the Photo model."""
-    class PhotoFactory(factory.django.DjangoModelFactory):
-        class Meta:
-            model = Photo
+    # class PhotoFactory(factory.django.DjangoModelFactory):
+    #     class Meta:
+    #         model = Photo
 
-        image = open(TEST_IMAGE_PATH)
-        author = 
-        description = fake.paragraph()
-        date_created = fake.date(pattern="%Y-%m-%d")
-        date_modified = fake.date(pattern="%Y-%m-%d")
-        date_published = ""
-        published = "private"
-        albums = ""
-
+    #     image = open(TEST_IMAGE_PATH)
+    #     author = 
+    #     description = fake.paragraph()
+    #     date_created = fake.date(pattern="%Y-%m-%d")
+    #     date_modified = fake.date(pattern="%Y-%m-%d")
+    #     date_published = ""
+    #     published = "private"
+    #     albums = ""
 
     def setUp(self):
         """The appropriate setup for the appropriate test."""
-        self.photos = [self.PhotoFactory.create() for i in range(20)]
-
+        self.new_user = User.objects.create_user("joe")
 
     def test_that_the_test_factory_works(self):
         """Test that this is happening."""
-        self.assertTrue(Photo.objects.count() == 20)
-
+        bob = User()
+        bob.username = "This Bob"
+        bob.save()
+        pic = Photo(author=bob)
+        pic.save()
+        self.assertTrue(pic.author.username == "This Bob")
