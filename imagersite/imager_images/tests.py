dc469f5bfdd8543685668bd41d5273c2ccd9dd80
Rick Valenzuela <devnull@rickv.com>
2017-01-19 18:01:17 -0800

    moved everything under testcase class

--- imagersite/imager_images/tests.py
+++ imagersite/imager_images/tests.py
@@ -3,20 +3,20 @@ from django.contrib.auth.models import User
 from imager_images.models import Photo, Album
 import factory
 import os
-from django.core.file import File
 from faker import Faker
 
 fake = Faker()
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEST_IMAGE_PATH = os.path.join(BASE_DIR, 'imager_images/test.png')
 
-# Assumes there is a test.png next to our tests.py
-TEST_IMAGE = os.path.join(os.path.dirname(__file__), 'test.png')
-
-class PhotoFactory(factory.django.DjangoModelFactory):
+class ImageTestCase(TestCase):
+    """The test runner for the Photo model."""
+    class PhotoFactory(factory.django.DjangoModelFactory):
         class Meta:
-            model = models.Photo
+            model = Photo
 
-        image = factory.LazyAttribute(lambda t: File(open(TEST_IMAGE)))
-        author = fake.name()
+        image = open(TEST_IMAGE_PATH)
+        author = 
         description = fake.paragraph()
         date_created = fake.date(pattern="%Y-%m-%d")
         date_modified = fake.date(pattern="%Y-%m-%d")
@@ -25,12 +25,12 @@ class PhotoFactory(factory.django.DjangoModelFactory):
         albums = ""
 
 
-def setUp(self):
-    """The appropriate setup for the appropriate test."""
-    self.photos = [self.PhotoFactory.create() for i in range(20)]
+    def setUp(self):
+        """The appropriate setup for the appropriate test."""
+        self.photos = [self.PhotoFactory.create() for i in range(20)]
 
 
-def test_that_the_test_factory_works(self):
-    """Test that this is happening."""
-    self.assertTrue(Photo.objects.count() == 20)
+    def test_that_the_test_factory_works(self):
+        """Test that this is happening."""
+        self.assertTrue(Photo.objects.count() == 20)
 
