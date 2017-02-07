c148a8b99984cd25ec106870884e1c2018252294
root <root@ip-172-31-29-239.us-west-2.compute.internal>
2017-02-07 22:00:32 +0000

    edited settings.py

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # FILE_UPLOAD_PERMISSIONS = 0644
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(" ")
 
