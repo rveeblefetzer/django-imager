ffdaea8514223aca4881e44692dea58815604ef6
Ted Callahan <CCallahanIV@gmail.com>
2017-02-10 15:02:06 -0800

    Fixed Album Serializer to produce list of pictures for each album item.

--- imagersite/imager_api/serializers.py
+++ imagersite/imager_api/serializers.py
@@ -7,18 +7,19 @@ from imager_images.models import Photo, Album
 class PhotoSerializer(serializers.HyperlinkedModelSerializer):
     """Define a serializer for the Photo object."""
 
-    author = serializers.ReadOnlyField(source='auther.username')
+    author = serializers.ReadOnlyField(source='author.username')
 
     class Meta:
         model = Photo
         fields = ('title', 'description', 'date_uploaded', 'published', 'author', 'image')
 
 
-class AlbumSerializer(serializers.HyperlinkedModelSerializer):
+class AlbumSerializer(serializers.Serializer):
     """Define serializer for album data."""
 
     owner = serializers.ReadOnlyField(source='owner.username')
+    pictures = PhotoSerializer(many=True)
 
     class Meta:
         model = Album
-        fields = ('title', 'description', 'date_created', 'published', 'owner', 'album_cover')
+        fields = ('title', 'description', 'date_created', 'published', 'owner', 'album_cover', 'pictures')
